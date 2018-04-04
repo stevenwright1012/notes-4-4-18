5024c44f6c07e50b71189b609e46d3383c7332fb
Steven <stevenwright1012@gmail.com>
2018-04-04 16:14:23 -0600

    end of day

---
+++ morning-lecture-notes.js
@@ -0,0 +1,27 @@
+//All data must be STORED, if even just for a moment
+
+
+//retrieve data
+    //often in the form of a question.
+
+//A database is an organized way to store and retrieve data
+//Schema is like a blue print for every row
+    //id INTERGER PRIMARY KEY,
+    //name TEXT,
+    //age INTEGER,
+    //country TEXT
+
+
+//types
+    //INTERGER
+    //REAL
+    //DECIMAL (scale, precision)
+    //TEXT (varchar)
+    //BLOB
+
+//SQL = Sturctured Query Lnaguage
+    //Create tables and rows
+    //read data out of tables
+    //update existing
+    //delete rows and columns
+
